39ba7fa45b7922110fd66a33a4cf226bc436c4a2
leo-i <lev_izjurov@rambler.ru>
2023-04-19 21:49:27 +0300

    finnaly it works

--- Makefile
+++ Makefile
@@ -10,7 +10,10 @@ KDIR = /media/program/embeddedLinux/buildroot/output/build/linux-custom/
 default:
 	clear
 	$(MAKE) -C $(KDIR) M=$(PWD) ARCH=arm CROSS_COMPILE=$(CROSS_COMPILE) modules
-	
+send:
+	ssh rpi 'rm chardev_driver.ko'
+	scp chardev_driver.ko rpi:/root
+
 clean:
 	$(MAKE) -C $(KDIR) M=$(PWD) ARCH=arm64 CROSS_COMPILE=$(CROSS_COMPILE) clean
 
--- chardev_driver.c
+++ chardev_driver.c
@@ -1,22 +1,26 @@
 #include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/platform_device.h>
-#include <linux/of.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/uaccess.h>
 
 #define PWM_DEV_NUM 1
 #define DEVICE_NAME "Custom_PWM_driver"
 #define CLASS_NAME  "Custom_PWM_class"
+
+#define BUFFER_SIZE 128
+
 
 static ssize_t PWM_read(struct file *filp, char *buffer, size_t length, loff_t *offset);
 static ssize_t PWM_write(struct file *filp, const char *buffer, size_t length, loff_t *offset);
 static int __init PMW_driver_init(void);
 static void __exit PMW_driver_exit(void);
 
-static struct class *custom_pwm_class;
-static struct cdev *pwm_cdev;
-static dev_t dev_num;
+static char device_buffer[BUFFER_SIZE];
+static struct class *custom_pwm_class; // Global variable for the device class
+static struct cdev pwm_cdev; // Global variable for the character device structure
+static dev_t dev_num; // Global variable for the first device number
 static struct file_operations PWM_driver_fops = 
 {
     .owner   = THIS_MODULE,
@@ -30,8 +34,25 @@ static struct file_operations PWM_driver_fops =
 // *pos indicates the start position from which data should be read in the file
 static ssize_t PWM_read(struct file *filp, char *buffer, size_t length, loff_t *offset){
 
-    printk("PWM driver read function called" );
-    return 4;
+    int maxbytes;           /* maximum bytes that can be read from ppos to BUFFER_SIZE*/
+    int bytes_to_read;      /* gives the number of bytes to read*/
+    int bytes_read;         /* number of bytes actually read*/
+    maxbytes = BUFFER_SIZE - *offset;
+    
+    if (maxbytes > length)
+        bytes_to_read = length;
+    else
+        bytes_to_read = maxbytes;
+    if (bytes_to_read == 0)
+        printk(KERN_INFO "charDev : Reached the end of the device\n");
+
+    bytes_read = bytes_to_read - copy_to_user(buffer, device_buffer + *offset, bytes_to_read);
+    printk(KERN_INFO "charDev : device has been read %d\n", bytes_read);
+
+    *offset += bytes_read;
+    printk(KERN_INFO "charDev : device has been read\n");
+
+    return bytes_read;
 }
 
 // *buf represents the data buffer coming from the user space.
@@ -39,54 +60,65 @@ static ssize_t PWM_read(struct file *filp, char *buffer, size_t length, loff_t *
 // *pos indicates the start position from which data should be written in the file
 static ssize_t PWM_write(struct file *filp, const char *buffer, size_t length, loff_t *offset){
 
-    printk("PWM driver write function called" );
-    return 5;
+    int maxbytes;           /* maximum bytes that can be read from ppos to BUFFER_SIZE*/
+    int bytes_to_write;     /* gives the number of bytes to write*/
+    int bytes_writen;       /* number of bytes actually writen*/
+    maxbytes = BUFFER_SIZE - *offset;
+    
+    if (maxbytes > length)
+        bytes_to_write = length;
+    else
+        bytes_to_write = maxbytes;
+
+    bytes_writen = bytes_to_write - copy_from_user(device_buffer + *offset, buffer, bytes_to_write);
+    printk(KERN_INFO "charDev : device has been written %d\n", bytes_writen);
+    
+    *offset += bytes_writen;
+    printk(KERN_INFO "charDev : device has been written\n");
+    
+    return bytes_writen;
 }
 
 static int __init PMW_driver_init(void){
-    int i;
-    dev_t curr_dev;
-    /* Request for a major and PWM_DEV_NUM minors */
-    alloc_chrdev_region(&dev_num, 0, PWM_DEV_NUM, DEVICE_NAME);
-
-    /* create our device class, visible in /sys/class */
-    custom_pwm_class = class_create(THIS_MODULE, CLASS_NAME);
-
-    /* Each device is represented as a character device (cdev) */
-    for (i = 0; i < PWM_DEV_NUM; i++) {
-
-        /* bind file_operations to the cdev */
-        cdev_init(&pwm_cdev[i], &PWM_driver_fops);
-        pwm_cdev[i].owner = THIS_MODULE;
-
-        /* Device number to use to add cdev to the core */
-        curr_dev = MKDEV(MAJOR(dev_num),
-        MINOR(dev_num) + i);
-
-        /* Make the device live for the users to access */
-        cdev_add(&pwm_cdev[i], curr_dev, 1);
-
-        /* create a node for each device */
-        device_create(custom_pwm_class,
-                        NULL, /* no parent device */
-                        curr_dev,
-                        NULL, /* no additional data */
-                        DEVICE_NAME "%d", i); /* eep-mem[0-7] */
-    }
     
-    printk("PWM driver module was loaded" );
-    return 0;
+    /* Request for a major and PWM_DEV_NUM minors */
+    if ( alloc_chrdev_region(&dev_num, 0, PWM_DEV_NUM, DEVICE_NAME) < 0 ) 
+        goto chardev_region_failed;
+
+    if (( custom_pwm_class = class_create(THIS_MODULE, CLASS_NAME)) == NULL)
+        goto class_failed;
+
+
+    if (device_create(custom_pwm_class, NULL, dev_num, NULL, DEVICE_NAME) == NULL)
+        goto device_failed;
+
+
+    cdev_init(&pwm_cdev, &PWM_driver_fops);
+    if (cdev_add(&pwm_cdev, dev_num, PWM_DEV_NUM) == -1)
+        goto cdev_failed;
+
+
+    pr_info("PWM driver module was loaded\n" );
+    return 0; 
+
+cdev_failed:
+    device_destroy(custom_pwm_class, dev_num);
+device_failed:
+    class_destroy(custom_pwm_class);
+class_failed:
+    unregister_chrdev_region(dev_num, PWM_DEV_NUM);
+chardev_region_failed:
+    return -1;       
 }
 
 static void __exit PMW_driver_exit(void){
-    int i;
-    for (i = 0; i < PWM_DEV_NUM; i++) {
-        device_destroy(custom_pwm_class, MKDEV(MAJOR(dev_num), (MINOR(dev_num) +i)));
-        cdev_del(&pwm_cdev[i]);
-    }
-    class_unregister(custom_pwm_class);
+
+    cdev_del(&pwm_cdev);
+    device_destroy(custom_pwm_class, dev_num );
     class_destroy(custom_pwm_class);
     unregister_chrdev_region(dev_num, PWM_DEV_NUM);
+    
+    pr_info("PWM driver module was unloaded\n" );
 
 }
 
